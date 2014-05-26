c7ae5a4f393773c5b26e0dcfe0ec8253a9f50936
Jackson Hong <jackson.hong@kontagent.com>
2014-05-26 17:03:24 -0400

    bleh

--- runserver.py
+++ runserver.py
@@ -1,4 +1,5 @@
 import os
+import json
 from flask import Flask
 
 import localsettings
@@ -7,9 +8,10 @@ app = Flask(__name__)
 app.debug = localsettings.DEBUG
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
-
+def index():
+    api_payload = open('api_payload.json', 'r')
+    api_data = api_payload.read()
+    return api_data
 
 if __name__ == '__main__':
     app.run()
