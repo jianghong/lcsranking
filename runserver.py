29c3da351224de613e3423320ddfb7cfdd2c8289
Jackson Hong <jackson.hong@kontagent.com>
2014-05-26 16:42:03 -0400

    add localsettings to runserver

--- runserver.py
+++ runserver.py
@@ -1,8 +1,15 @@
 import os
 from flask import Flask
 
+import localsettings
+
 app = Flask(__name__)
+app.debug = localsettings.DEBUG
 
 @app.route('/')
 def hello():
     return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
