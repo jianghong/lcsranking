6ce7a3b97cd2dcbb2515bd18dcb2bf580cce0a38
Jackson Hong <jackson.hong@kontagent.com>
2014-05-26 20:34:10 -0400

    add sort and +/-

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -4,7 +4,8 @@ require.config({
 		'underscore': 'lib/underscore-min',
 		'backbone': 'lib/backbone-min',
 		'bootstrap': 'lib/bootstrap.min',
-		'jquery': 'lib/jquery.min'
+		'jquery': 'lib/jquery.min',
+		'jquery-tablesorter': 'lib/jquery.tablesorter.min'
 	},
 	shim: {
 		'underscore': {
@@ -13,15 +14,29 @@ require.config({
 		'backbone': {
 			deps: ['underscore', 'jquery'],
 			exports: 'Backbone'
+		},
+		'jquery-tablesorter': {
+			deps: ['jquery']
+		},
+		'bootstrap': {
+			deps: ['jquery']
 		}
 	}
 });
 
-require(['jquery'], function($){
+require(['jquery', 'jquery-tablesorter'], function($){
+	var generate_index = function() {
+		$.each($('.index'), function(i, v){
+			$(this).html(i+1);
+		});
+	};
+
 	var tap, tpp;
 	$.each($('.eff'), function(i, v){
 		tap = $(v).siblings('.tap').html();
 		tpp = $(v).siblings('.tpp').html();
-		$(this).html((tap / tpp).toFixed(2));
+		$(this).html(((tap / tpp) - 1.00).toFixed(2));
 	});
+	$("#main-table").tablesorter( {sortList: [[2], [1,0]]} );
+	generate_index();
 });
